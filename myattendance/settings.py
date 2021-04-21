f992fabcd80f8d57a5131093af2577fd4d753c42
Fakhre Alam <fakhrealam8573@gmail.com>
2021-04-21 19:33:04 +0530

    Update settings.py

--- myattendance/settings.py
+++ myattendance/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '78325ae2e8c5dc89e031d6cb9dfe0401ca51593a5cfa9274'
-SECRET_KEY = os.environ.get(SECRET_KEY)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
