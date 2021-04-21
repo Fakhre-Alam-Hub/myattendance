cd9c1d43bb0f581c4beed05fe9d172f0d98748f2
Fakhre Alam <fakhrealam8573@gmail.com>
2021-04-21 19:58:12 +0530

    Update settings.py

--- myattendance/settings.py
+++ myattendance/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
+# DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['myattendance2021.herokuapp.com']
 
